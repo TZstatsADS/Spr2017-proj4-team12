edb399909351562cd7d567e85a9d788d33ed35ed
Chenyun Zhu <cz2434@columbia.edu>
2017-04-11 17:12:05 -0400

    Bug fix

--- lib/feature_paper5.R
+++ lib/feature_paper5.R
@@ -1,38 +1,4 @@
-## data = cluster_merge(AKumar, rep(1:4,61))
-cosine_similarity <- function(data){
-  
-  if (!require("text2vec")) install.packages("text2vec")
-  if (!require("qlcMatrix")) install.packages("qlcMatrix")
-  library(text2vec)
-  library(qlcMatrix)
-  
-  feature <- list()
-  for (i in 1:3){
-  it_train <- itoken(data$Clustered[,i],
-                     preprocessor = tolower,
-                     tokenizer = word_tokenizer,
-                     # ids = data$PaperID,
-                     # turn off progressbar because it won't look nice in rmd
-                     progressbar = FALSE)
-  vocab <- create_vocabulary(it_train, stopwords = c("a", "an", "the", "in", "on",
-                                                     "at", "of", "above", "under"))
-  #vocab
-  vectorizer <- vocab_vectorizer(vocab)
-  dtm_train <- create_dtm(it_train, vectorizer)
-  tfidf <- TfIdf$new()
-  dtm_train_tfidf <- fit_transform(dtm_train, tfidf)
-  ftfidf<-as.matrix(dtm_train_tfidf)
-  feature[[i]] <- as.matrix(cosSparse(t(ftfidf)))
-}
-  # feature$label<-factor(data$AuthorID)
-  return(list(CLUSTER.ID = as.numeric(rownames(feature[[1]])), MATRIX = feature))
-}
-
-# AKumar <- read.csv("AKumar.csv", as.is = T)
-# # # # "as.is" is important here.
-# test <- cosine_similarity(AKumar)
-# raw_data <- AKumar[,2:4]
-
+Sys.setlocale("LC_ALL", "C")
 
 mergesomerows  <- function(cluster.id, mergevector, label_hat){
   paste(mergevector[cluster.id == label_hat], collapse = " ")
@@ -59,6 +25,41 @@ cluster_merge <- function(raw_data, label_hat){
   return(list(Clustered = merged_matrix, matrix_label = unique_label_hat))
 }
 
+## data = cluster_merge(AKumar, rep(1:4,61))
+## cluster_merge(CChen, rep(1:3,267))
+cosine_similarity <- function(data){
+  
+  if (!require("text2vec")) install.packages("text2vec")
+  if (!require("qlcMatrix")) install.packages("qlcMatrix")
+  library(text2vec)
+  library(qlcMatrix)
+  
+  feature <- list()
+  for (i in 1:3){
+    it_train <- itoken(data$Clustered[,i],
+                       preprocessor = tolower,
+                       tokenizer = word_tokenizer,
+                       # ids = data$PaperID,
+                       # turn off progressbar because it won't look nice in rmd
+                       progressbar = FALSE)
+    vocab <- create_vocabulary(it_train, stopwords = c("a", "an", "the", "in", "on",
+                                                       "at", "of", "above", "under"))
+    #vocab
+    vectorizer <- vocab_vectorizer(vocab)
+    dtm_train <- create_dtm(it_train, vectorizer)
+    tfidf <- TfIdf$new()
+    dtm_train_tfidf <- fit_transform(dtm_train, tfidf)
+    ftfidf<-as.matrix(dtm_train_tfidf)
+    feature[[i]] <- as.matrix(cosSparse(t(ftfidf)))
+  }
+  # feature$label<-factor(data$AuthorID)
+  return(list(CLUSTER.ID = data$matrix_label, MATRIX = feature))
+}
+
+AKumar <- read.csv("AKumar.csv", as.is = T)
+CChen <- read.csv("CChen.csv", as.is = T)
+# # # "as.is" is important here.
+test <- cosine_similarity(cluster_merge(CChen, rep(1:4,61)))
 
 
 
@@ -115,3 +116,6 @@ cluster_merge <- function(raw_data, label_hat){
 # }
 
 # AKumar.feature.new <- feature_paper5_Paper.Journal(AKumar)
+
+
+
