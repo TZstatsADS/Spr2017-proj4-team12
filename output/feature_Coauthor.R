78be330c829a56c6172988b0583e57cfa517a103
Luffy-Han <2862062780@qq>
2017-04-12 18:42:18 -0400

    svm update
    svm update

---
+++ output/feature_Coauthor.R
@@ -0,0 +1,26 @@
+#input is dataframe
+get_feature_coauthor<-function(data){
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(text2vec, dplyr, qlcMatrix, kernlab, knitr)
+  it_train <- itoken(data$Coauthor,
+                     preprocessor = tolower,
+                     tokenizer = word_tokenizer,
+                     ids = data$PaperID,
+                     # turn off progressbar because it won't look nice in rmd
+                     progressbar = FALSE)
+  vocab <- create_vocabulary(it_train, stopwords = c("a", "an", "the", "in", "on",
+                                                     "at", "of", "above", "under"))
+  #vocab
+  vectorizer <- vocab_vectorizer(vocab)
+  dtm_train <- create_dtm(it_train, vectorizer)
+  tfidf <- TfIdf$new()
+  dtm_train_tfidf <- fit_transform(dtm_train, tfidf)
+  docsdissim <- cosSparse(t(dtm_train_tfidf))
+  y<-as.data.frame(as.matrix(docsdissim))
+  y$label<-as.factor(data$AuthorID)
+  return(y)
+  #result_sclust <- specc(as.matrix(dtm_train_tfidf),
+  # centers=length(unique(data$AuthorID)))
+  #return(result_sclust)
+  
+}
